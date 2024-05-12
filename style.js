a85560ece987d4da6dba6fcb48c78d68cdc64124
Rahul20231045 <130646031+Rahul20231045@users.noreply.github.com>
2024-05-12 23:57:31 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,13 @@
+document.querySelector('.menu-bar').addEventListener('click', function() {
+    this.classList.toggle('open');
+  });
+  
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
